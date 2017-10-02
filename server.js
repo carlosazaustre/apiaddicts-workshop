d02cac8d4a745c0f8111f9ec1261b69fad3dd90c
Carlos Azaustre <cazaustre@gmail.com>
2017-10-02 09:21:55 +0200

    add security

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const http = require('http')
 const express = require('express')
 const asyncify = require('express-asyncify')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
 const api = require('./api')
 
 const port = process.env.PORT || 3000
@@ -13,12 +14,18 @@ const server = http.createServer(app)
 
 mongoose.Promise = Promise
 
+// help protect your app from some well-known web vulnerabilities by setting HTTP headers appropriately
+app.use(helmet())
+
+// All API endpoint routes are in /api/<endpoint>
 app.use('/api', api)
 
+// Connect to database
 mongoose.connect('mongodb://localhost/apiaddicts', {
   useMongoClient: true
 })
 
+// Run Express server
 server.listen(port, () => {
   debug(`server running on port ${port}`)
 })
